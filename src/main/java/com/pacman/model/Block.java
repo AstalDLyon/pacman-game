c441a50f4e26eaeca0fab600c8ef527c44b290c5
Breno Alves <bralves@hotmail.com.br>
2026-06-18 22:27:54 -0300

    Enumeração e Variaveis inicias na tentativa de criar um PacMan

---
+++ src/main/java/com/pacman/model/Block.java
@@ -0,0 +1,12 @@
+package com.pacman.model;
+
+public sealed interface Block{
+    Position position ();
+    record Wall(Position position) implements Block{}
+
+    record Food(Position position) implements Block{}
+
+    record PacMan(Position position, Direction direction) implements Block{}
+
+    record Ghost(Position position, Direction direction, GhostType type) implements Block {}
+}
---
+++ src/main/java/com/pacman/model/Position.java
@@ -0,0 +1,7 @@
+package com.pacman.model;
+
+public record Position(int x, int y) {
+    public Position move (int deltaX, int deltaY) {
+        return new Position(x + deltaX, y + deltaY);
+    }
+}
