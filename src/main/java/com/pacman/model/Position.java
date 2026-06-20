8ce5ed6c215f0ee3ad54a2e48cf28fc2758b8478
Breno Alves <bralves@hotmail.com.br>
2026-06-20 19:25:57 -0300

    Testando

--- src/main/java/com/pacman/model/Position.java
+++ src/main/java/com/pacman/model/Position.java
@@ -3,5 +3,6 @@ package com.pacman.model;
 public record Position(int x, int y) {
     public Position move (int deltaX, int deltaY) {
         return new Position(x + deltaX, y + deltaY);
+        // Teste de pull
     }
 }
